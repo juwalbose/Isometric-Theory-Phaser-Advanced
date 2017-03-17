a934decbbe1c885c107deb63503ec75cf2b6c885
Juwal Bose <juwalbose@gmail.com>
2017-03-17 13:16:11 +0000

    path finding tutorial completed

--- src/isometricpathfind.js
+++ src/isometricpathfind.js
@@ -48,8 +48,10 @@ var easystar;
 var isFindingPath=false;
 var path=[];
 var destination=heroMapTile;
-var stepsTillTurn=15;
+var stepsTillTurn=19;//20 works best but thats for full frame rate
 var stepsTaken=0;
+var isWalking;
+var halfSpeed=0.8;//changed from 0.5 for smooth diagonal walks
 
 function preload() {
     //load all necessary assets
@@ -76,13 +78,14 @@ function create() {
     sorcererShadow=game.make.sprite(0,0,'heroShadow');
     sorcererShadow.scale= new Phaser.Point(0.5,0.6);
     sorcererShadow.alpha=0.4;
+    isWalking=false;
     createLevel();
     
     easystar = new EasyStar.js();
     easystar.setGrid(levelData);
     easystar.setAcceptableTiles([0]);
-    easystar.enableDiagonals();
-    easystar.disableCornerCutting();
+    easystar.enableDiagonals();// we want path to have diagonals
+    easystar.disableCornerCutting();// no diagonal path when walking at wall corners
     
     game.input.activePointer.leftButton.onUp.add(findPath)
 }
@@ -101,17 +104,15 @@ function update(){
         }
     }
     //check if we are walking into a wall else move hero in 2D
-    if (isWalkable())
-    {
-        heroMapPos.x +=  heroSpeed * dX;
-        heroMapPos.y +=  heroSpeed * dY;
-        heroMapSprite.x=heroMapPos.x-heroMapSprite.width/2;
-        heroMapSprite.y=heroMapPos.y-heroMapSprite.height/2;
-        //get the new hero map tile
-        heroMapTile=getTileCoordinates(heroMapPos,tileWidth);
-        //depthsort & draw new scene
-        renderScene();
-    }
+    
+    heroMapPos.x +=  heroSpeed * dX;
+    heroMapPos.y +=  heroSpeed * dY;
+    heroMapSprite.x=heroMapPos.x-heroMapSprite.width/2;
+    heroMapSprite.y=heroMapPos.y-heroMapSprite.height/2;
+    //get the new hero map tile
+    heroMapTile=getTileCoordinates(heroMapPos,tileWidth);
+    //depthsort & draw new scene
+    renderScene();
 }
 
 function createLevel(){//create minimap
@@ -197,7 +198,7 @@ function drawTileIso(tileType,i,j){//place isometric level tiles
     }
 }
 function findPath(){
-    if(isFindingPath)return;
+    if(isFindingPath || isWalking)return;
     var pos=game.input.activePointer.position;
     var isoPt= new Phaser.Point(pos.x-borderOffset.x,pos.y-borderOffset.y);
     tapPos=isometricToCartesian(isoPt);
@@ -207,7 +208,7 @@ function findPath(){
     if(tapPos.x>-1&&tapPos.y>-1&&tapPos.x<7&&tapPos.y<7){//tapped within grid
         if(levelData[tapPos.y][tapPos.x]!=1){//not wall tile
             isFindingPath=true;
-            
+            //let the algorithm do the magic
             easystar.findPath(heroMapTile.x, heroMapTile.y, tapPos.x, tapPos.y, plotAndMove);
             easystar.calculate();
         }
@@ -228,9 +229,8 @@ function plotAndMove(newPath){
         {
             var tmpSpr=minimap.getByName("tile"+path[i].y+"_"+path[i].x);
             tmpSpr.tint=0x0000ff;
-            console.log("p "+path[i].x+":"+path[i].y);
+            //console.log("p "+path[i].x+":"+path[i].y);
         }
-        
         
     }
 }
@@ -244,90 +244,17 @@ function repaintMinimap(){
         }
     }
 }
-function isWalkable(){//It is not advisable to create points in update loop, but for code readability.
-    var able=true;
-    var heroCornerPt=new Phaser.Point(heroMapPos.x-heroMapSprite.width/2,heroMapPos.y-heroMapSprite.height/2);
-    var cornerTL =new Phaser.Point();
-    cornerTL.x=heroCornerPt.x +  (heroSpeed * dX);
-    cornerTL.y=heroCornerPt.y +  (heroSpeed * dY);
-    // now we have the top left corner point. we need to find all 4 corners based on the map marker graphics width & height
-    //ideally we should just provide the hero a volume instead of using the graphics' width & height
-    var cornerTR =new Phaser.Point();
-    cornerTR.x=cornerTL.x+heroMapSprite.width;
-    cornerTR.y=cornerTL.y;
-    var cornerBR =new Phaser.Point();
-    cornerBR.x=cornerTR.x;
-    cornerBR.y=cornerTL.y+heroMapSprite.height;
-    var cornerBL =new Phaser.Point();
-    cornerBL.x=cornerTL.x;
-    cornerBL.y=cornerBR.y;
-    var newTileCorner1;
-    var newTileCorner2;
-    var newTileCorner3=heroMapPos;
-    //let us get which 2 corners to check based on current facing, may be 3
-    switch (facing){
-        case "north":
-            newTileCorner1=cornerTL;
-            newTileCorner2=cornerTR;
-        break;
-        case "south":
-            newTileCorner1=cornerBL;
-            newTileCorner2=cornerBR;
-        break;
-        case "east":
-            newTileCorner1=cornerBR;
-            newTileCorner2=cornerTR;
-        break;
-        case "west":
-            newTileCorner1=cornerTL;
-            newTileCorner2=cornerBL;
-        break;
-        case "northeast":
-            newTileCorner1=cornerTR;
-            newTileCorner2=cornerBR;
-            newTileCorner3=cornerTL;
-        break;
-        case "southeast":
-            newTileCorner1=cornerTR;
-            newTileCorner2=cornerBR;
-            newTileCorner3=cornerBL;
-        break;
-        case "northwest":
-            newTileCorner1=cornerTR;
-            newTileCorner2=cornerBL;
-            newTileCorner3=cornerTL;
-        break;
-        case "southwest":
-            newTileCorner1=cornerTL;
-            newTileCorner2=cornerBR;
-            newTileCorner3=cornerBL;
-        break;
-    }
-    //check if those corners fall inside a wall after moving
-    newTileCorner1=getTileCoordinates(newTileCorner1,tileWidth);
-    if(levelData[newTileCorner1.y][newTileCorner1.x]==1){
-        able=false;
-    }
-    newTileCorner2=getTileCoordinates(newTileCorner2,tileWidth);
-    if(levelData[newTileCorner2.y][newTileCorner2.x]==1){
-        able=false;
-    }
-    newTileCorner3=getTileCoordinates(newTileCorner3,tileWidth);
-    if(levelData[newTileCorner3.y][newTileCorner3.x]==1){
-        able=false;
-    }
-    return able;
-}
 function aiWalk(){
     if(path.length==0){//path has ended
         if(heroMapTile.x==destination.x&&heroMapTile.y==destination.y){
             dX=0;
             dY=0;
             //console.log("ret "+destination.x+" ; "+destination.y+"-"+heroMapTile.x+" ; "+heroMapTile.y);
-        
+            isWalking=false;
             return;
         }
     }
+    isWalking=true;
     if(heroMapTile.x==destination.x&&heroMapTile.y==destination.y){//reached current destination, set new, change direction
         //wait till we are few steps into the tile before we turn
         stepsTaken++;
@@ -335,15 +262,14 @@ function aiWalk(){
             return;
         }
         console.log("at "+heroMapTile.x+" ; "+heroMapTile.y);
-            
-        //heroMapSprite.x=(heroMapTile.x * tileWidth)+(tileWidth/2)-(heroMapSprite.width/2);
-        //heroMapSprite.y=(heroMapTile.y * tileWidth)+(tileWidth/2)-(heroMapSprite.height/2);
-        //heroMapPos.x=heroMapSprite.x+heroMapSprite.width/2;
-        //heroMapPos.y=heroMapSprite.y+heroMapSprite.height/2;
+        //centralise the hero on the tile    
+        heroMapSprite.x=(heroMapTile.x * tileWidth)+(tileWidth/2)-(heroMapSprite.width/2);
+        heroMapSprite.y=(heroMapTile.y * tileWidth)+(tileWidth/2)-(heroMapSprite.height/2);
+        heroMapPos.x=heroMapSprite.x+heroMapSprite.width/2;
+        heroMapPos.y=heroMapSprite.y+heroMapSprite.height/2;
         
         stepsTaken=0;
-        destination=path.pop();
-        //console.log("new "+destination.x+" ; "+destination.y);
+        destination=path.pop();//whats next tile in path
         if(heroMapTile.x<destination.x){
             dX = 1;
         }else if(heroMapTile.x>destination.x){
@@ -363,62 +289,62 @@ function aiWalk(){
         }else if(heroMapTile.y==destination.y){//left or right
             dY=0;
         }
-    }
-    
-   
-    if (dX==1)
-    {
-        if (dY == 0)
+        //figure out which direction to face
+        if (dX==1)
         {
-            facing = "east";
+            if (dY == 0)
+            {
+                facing = "east";
+            }
+            else if (dY==1)
+            {
+                facing = "southeast";
+                dX = dY=halfSpeed;
+            }
+            else
+            {
+                facing = "northeast";
+                dX=halfSpeed;
+                dY=-1*halfSpeed;
+            }
         }
-        else if (dY==1)
+        else if (dX==-1)
         {
-            facing = "southeast";
-            dX = dY=0.5;
-        }
-        else
-        {
-            facing = "northeast";
-            dX=0.5;
-            dY=-0.5;
-        }
-    }
-    else if (dX==-1)
-    {
-        dX = -1;
-        if (dY == 0)
-        {
-            facing = "west";
-        }
-        else if (dY==1)
-        {
-            facing = "southwest";
-            dY=0.5;
-            dX=-0.5;
-        }
-        else
-        {
-            facing = "northwest";
-            dX = dY=-0.5;
-        }
-    }
-    else
-    {
-        dX = 0;
-        if (dY == 0)
-        {
-            //facing="west";
-        }
-        else if (dY==1)
-        {
-            facing = "south";
+            dX = -1;
+            if (dY == 0)
+            {
+                facing = "west";
+            }
+            else if (dY==1)
+            {
+                facing = "southwest";
+                dY=halfSpeed;
+                dX=-1*halfSpeed;
+            }
+            else
+            {
+                facing = "northwest";
+                dX = dY=-1*halfSpeed;
+            }
         }
         else
         {
-            facing = "north";
+            dX = 0;
+            if (dY == 0)
+            {
+                //facing="west";
+            }
+            else if (dY==1)
+            {
+                facing = "south";
+            }
+            else
+            {
+                facing = "north";
+            }
         }
     }
+    //console.log(facing);
 }
 
 function cartesianToIsometric(cartPt){
